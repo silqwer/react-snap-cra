3b65ffede090cb5612c9280cb377061d2f2c99c6
silqwer <silqwer@gmail.com>
2024-02-15 14:29:20 +0900

    feat: tag2

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Tag from "./pages/Tag";
 import Tag1 from "./pages/Tag1";
+import Tag2 from "./pages/Tag2";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/tag" element={<Tag />} />
       <Route path="/tag1" element={<Tag1 />} />
+      <Route path="/tag2" element={<Tag2 />} />
     </Routes>
   );
 }
---
+++ src/pages/Tag2.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from "react-helmet-async";
+import { useSearchParams } from "react-router-dom";
+
+export default function Tag1() {
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title") as string;
+  const image = searchParams.get("image") as string;
+
+  return (
+    <div>
+      <Helmet>
+        <title>Tag2</title>
+        <meta property="og:site_name" content="TwinWorld" />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content="https://twin.world" />
+        <meta property="og:description" content="설명 테스트입니다." />
+        <meta property="og:image" content={image} />
+      </Helmet>
+      <h1>Tag1</h1>
+      <div>{title}</div>
+      <div>{image}</div>
+    </div>
+  );
+}
